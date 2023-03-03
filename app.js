00f5ff2a311735040a691d011cf3e4e326b87921
ochiroo <eochir844@gmail.com>
2023-03-03 17:35:18 +0800

    Shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -12,19 +12,25 @@ var roundScore = 0;
 
 // Shoonii ali talaaraa buusniig hadgalah huvisagch, 1-6 hurtel too sanamsarguigeer ugnu
 
-var dice = Math.floor(Math.random() * 6) + 1;
 
 // id="score-0" player 1
 // document.querySelector('#score-0').textContent = dice;
 
 // player 2
 // document.querySelector('#score-1').innerHTML = "<em>Yes</em>";
+
 //Programm ehlehed beltey
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
 
-document.querySelector('.dice').style.display = "none";
-document.querySelector('#score-0').textContent = 0;
-document.querySelector('#score-1').textContent = 0;
-document.querySelector('#current-0').textContent = 0;
-document.querySelector('#current-1').textContent = 0;
+var diceDom = document.querySelector('.dice');
+diceDom.style.display = "none";
 
-console.log("Shoo " +  dice);
+document.querySelector('.btn-roll').addEventListener("click", function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+    diceDom.style.display = "block";
+    diceDom.src="dice-" + diceNumber + ".png";
+});
+
