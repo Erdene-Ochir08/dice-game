363a81ae4e4ae9b6e09d34435c1b160210fd66a5
ochiroo <eochir844@gmail.com>
2023-03-04 16:42:55 +0800

    Toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // Toglogchiin eeljiig hadgalah huvisagch, negduger toglogch 0, hoyordugaar toglogch 1 
 
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Toglogchiin tsugluulsan onoog hadglahah huvisagch
 
@@ -27,10 +27,39 @@ document.getElementById('current-1').textContent = '0';
 
 var diceDom = document.querySelector('.dice');
 diceDom.style.display = "none";
-
+// Shoog shideh eventListener
 document.querySelector('.btn-roll').addEventListener("click", function(){
+    // 1-6 dotorh sanamsargui toog gargaj avna
     var diceNumber = Math.floor(Math.random() * 6) + 1;
+    // shoonii zurgiig web deer gargaj irne
     diceDom.style.display = "block";
+    //Buusan sanamsargui toond hargalzah toonii zurgiig web deer gargaj irne
     diceDom.src="dice-" + diceNumber + ".png";
+    //Buusan too n 1ees yalgaatai bol idevhtei toglogchiin eeljiin onoog nemegduulne
+    if(diceNumber !== 1){
+        //1-ees yalgaatai too buulaa. Buusan toog toglogchid nemne
+        roundScore = roundScore + diceNumber;
+        document.getElementById('current-' + activePlayer).textContent = roundScore;
+    }else{
+        //Vgui bol idevhtei idevhtei toglogchiin 1 bolgo
+        //Ene toglogchiin eeljin deer tsugluulsan onoog 0 bolgo
+        roundScore = 0;
+        document.getElementById('current-' + activePlayer).textContent = 0;
+        //1 buusan bol toglogchiin eeljiig ene hesegt solij ugnu
+        //Herev idevhtei toglogch n 0 baival idevhtei toglogchiin 1 bolgo
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        //Ulaan tsegiig shiljvvleh 
+        document.querySelector('.player-0-panel').classList.toggle("active");
+        document.querySelector('.player-1-panel').classList.toggle("active");
+        //Shoog tur alga bolgono
+        diceDom.style.display = "none";
+        // if(activePlayer === 0){
+        //     activePlayer = 1;
+        // }else{
+        //     activePlayer = 0;
+        // }
+    }
+
+
 });
 
